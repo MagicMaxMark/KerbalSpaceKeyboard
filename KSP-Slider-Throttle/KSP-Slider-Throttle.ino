e9d3302018cacc664b4bf36345cd869a4ce81718
Alex <alexanderesolis@gmail.com>
2020-01-23 15:33:20 -0500

    started programming the throttle

--- KSP-Slider-Throttle/KSP-Slider-Throttle.ino
+++ KSP-Slider-Throttle/KSP-Slider-Throttle.ino
@@ -1,9 +1,32 @@
+#include "Keyboard.h"
+
+int pos = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  Keyboard.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (pos < map(analogRead(2), 0, 1023, 0, 10)){
+    pos ++;
+    Keyboard.press(KEY_LEFT_SHIFT);
+    delay(40);
+    Keyboard.release(KEY_LEFT_SHIFT);
+  }
+  while (pos > map(analogRead(2), 0, 1023, 0, 10)){
+    pos --;
+    Keyboard.print(KEY_LEFT_CTRL);
+    delay(40);
+    Keyboard.release(KEY_LEFT_CTRL);
+  }
 
+  if (pos == 0){
+    Serial.println("z");
+  }
+  if (pos == 10){
+    Serial.println("x");
+  }
 }
