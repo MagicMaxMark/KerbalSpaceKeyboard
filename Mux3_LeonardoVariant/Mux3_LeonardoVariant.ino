619eae2050d8669fc7feb0a5f3ef2bdbf4f70559
magicmaxmark <magicmaxmark@gmail.com>
2020-01-30 11:18:42 -0500

    Modified for Leonardo

--- Mux3_LeonardoVariant/Mux3_LeonardoVariant.ino
+++ Mux3_LeonardoVariant/Mux3_LeonardoVariant.ino
@@ -1,24 +1,43 @@
+#include <Keyboard.h>
+
+#define muxZeroInput A0
+#define muxZeroOutputZero 2
+#define muxZeroOutputOne 3
+#define muxZeroOutputTwo 4
+
+#define muxOneInput A1
+#define muxOneOutputZero 5
+#define muxOneOutputOne 6
+#define muxOneOutputTwo 7
+
+#define muxTwoInput A2
+#define muxTwoOutputZero 8
+#define muxTwoOutputOne 9
+#define muxTwoOutputTwo 10
+
+#define muxThreeInput A3
+#define muxThreeOutputZero 14
+#define muxThreeOutputOne 15
+#define muxThreeOutputTwo 16
+
 int bits[] = {0,0,0};
+char keys[] = {'c', 'm', 'g', 'u', KEY_BACKSPACE, ' ', 't', 'r'};
+bool buttonsPressed[] = {false, false, false, false, false, false, false, false};
 
 void setup(){
-  //Keyboard.begin
+  Keyboard.begin();
   Serial.begin(9600);
   Serial.println("Hello World");
 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
-  pinMode(5, INPUT);
+  pinMode(A0, INPUT);
 }
 
 void loop(){
   if(digitalRead(6) != 0){
     muxOneLoop();
-  }
-
-  else{
-    Serial.print("Broken :( ");
-    Serial.println(digitalRead(6));
   }
 }
 
@@ -34,9 +53,13 @@ void checkButton(int button){
   digitalWrite(3, bits[1]);
   digitalWrite(4, bits[2]);
 
-  if(digitalRead(5)){
-    Serial.print(button);
-    Serial.println(": true ");
+  if(digitalRead(A0) && !buttonsPressed[button]){
+    Keyboard.print(keys[button]);
+    buttonsPressed[button] = true;
+  }
+
+  else if(buttonsPressed[button] && !digitalRead(A0)){
+    buttonsPressed[button] = false;
   }
 }
 
@@ -52,4 +75,5 @@ void DecimalToBinary(int n) {
    bits[0] = binaryNumber[0];
    bits[1] = binaryNumber[1];
    bits[2] = binaryNumber[2];
+   //
 } 
