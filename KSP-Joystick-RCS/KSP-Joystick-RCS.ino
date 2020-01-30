f967606466ce7a8eaf5032e658ff2ac5207fe1d2
magicmaxmark <magicmaxmark@gmail.com>
2020-01-30 13:26:48 -0500

    Added Variables

--- KSP-Joystick-RCS/KSP-Joystick-RCS.ino
+++ KSP-Joystick-RCS/KSP-Joystick-RCS.ino
@@ -1,5 +1,8 @@
 #include "Keyboard.h"
 
+int keyPressDelayMin = 60;
+int keyPressDelayMax = 200;
+
 void setup() {
   // open the serial port:
   Serial.begin(9600);
@@ -10,25 +13,25 @@ void setup() {
 void loop() {
   if (analogRead(3) > 621) {
     Keyboard.print("j");
-    delay(map(analogRead(3), 621, 1023, 200, 60));
+    delay(map(analogRead(3), 621, 1023, keyPressDelayMax, keyPressDelayMin));
   } else if (analogRead(3) < 419) {
     Keyboard.print("l");
-    delay(map(analogRead(3), 0, 419, 60, 200));
+    delay(map(analogRead(3), 0, 419, keyPressDelayMin, keyPressDelayMax));
   }
   
   if (analogRead(4) > 603) {
     Keyboard.print("n");
-    delay(map(analogRead(4), 603, 1023, 200, 60));
+    delay(map(analogRead(4), 603, 1023, keyPressDelayMax, keyPressDelayMin));
   } else if (analogRead(4) < 401) {
     Keyboard.print("h");
-    delay(map(analogRead(4), 0, 401, 60, 200));
+    delay(map(analogRead(4), 0, 401, keyPressDelayMin, keyPressDelayMax));
   }
 
   if (analogRead(5) > 603) {
     Keyboard.print("i");
-    delay(map(analogRead(5), 603, 1023, 200, 60));
+    delay(map(analogRead(5), 603, 1023, keyPressDelayMax, keyPressDelayMin));
   } else if (analogRead(5) < 401) {
     Keyboard.print("k");
-    delay(map(analogRead(5), 0, 401, 60, 200));
+    delay(map(analogRead(5), 0, 401, keyPressDelayMin, keyPressDelayMax));
   }
 }
