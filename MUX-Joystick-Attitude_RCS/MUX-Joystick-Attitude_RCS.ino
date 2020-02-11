ba759cd9b441d6e7c53b39eb4b81215f993a16ab
Alex <alexanderesolis@gmail.com>
2020-02-11 10:19:50 -0500

    Create MUX-Joystick-Attitude_RCS.ino

---
+++ MUX-Joystick-Attitude_RCS/MUX-Joystick-Attitude_RCS.ino
@@ -0,0 +1,121 @@
+#include "Keyboard.h"
+int bits[] = {0,0,0};
+
+bool caps_lock_right = false;
+bool caps_lock_left  = false;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.print("Hello World");
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(A0, INPUT);
+
+  Keyboard.begin();
+}
+
+void loop(){
+  muxOneLoop();
+}
+void muxOneLoop(){
+  for(int i = 0; i < 8; i ++){
+    checkJoystick(i);
+  }
+}
+
+void checkJoystick(int axis){
+  DecimalToBinary(axis);
+  digitalWrite(4, bits[0]); //4ths place
+  digitalWrite(3, bits[1]); //2nds place
+  digitalWrite(2, bits[2]); //1sts place
+
+  if (axis == 0 && analogRead(A0) > 721){
+    Keyboard.press('j');
+  } else if (axis == 0 && analogRead(A0) < 319){
+    Keyboard.press('l');
+  } else if (axis == 0 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('j');
+    Keyboard.release('l');
+  }
+
+  if (axis == 1 && analogRead(A0) > 721){
+    Keyboard.press('n');
+  } else if (axis == 1 && analogRead(A0) < 319){
+    Keyboard.press('h');
+  } else if (axis == 1 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('n');
+    Keyboard.release('h');
+  }
+
+  if (axis == 2 && analogRead(A0) > 721){
+    Keyboard.press('i');
+  } else if (axis == 2 && analogRead(A0) < 319){
+    Keyboard.press('k');
+  } else if (axis == 2 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('i');
+    Keyboard.release('k');
+  }
+
+  if (axis == 3 && analogRead(A0) > 721){
+    Keyboard.press('s');
+  } else if (axis == 3 && analogRead(A0) < 319){
+    Keyboard.press('w');
+  } else if (axis == 3 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('s');
+    Keyboard.release('w');
+  }
+
+  if (axis == 4 && analogRead(A0) > 721){
+    Keyboard.press('a');
+  } else if (axis == 4 && analogRead(A0) < 319){
+    Keyboard.press('d');
+  } else if (axis == 4 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('a');
+    Keyboard.release('d');
+  }
+
+  if (axis == 5 && analogRead(A0) > 721){
+    Keyboard.press('q');
+  } else if (axis == 5 && analogRead(A0) < 319){
+    Keyboard.press('e');
+  } else if (axis == 5 && 319 < analogRead(A0) && analogRead(A0) < 721){
+    Keyboard.release('q');
+    Keyboard.release('e');
+  }
+
+  if ((axis == 6) && digitalRead(A0) == LOW && caps_lock_right == HIGH){
+    Keyboard.press(KEY_CAPS_LOCK);
+  }
+  if ((axis == 6) && digitalRead(A0) == HIGH && caps_lock_right == LOW){
+    Keyboard.release(KEY_CAPS_LOCK);
+  }
+  if (axis == 6){
+    caps_lock_right = digitalRead(A0);
+  }
+  
+  if ((axis == 7) && digitalRead(A0) == LOW && caps_lock_left == HIGH){
+    Keyboard.press(KEY_CAPS_LOCK);
+  }
+  if ((axis == 7) && digitalRead(A0) == HIGH && caps_lock_left == LOW){
+    Keyboard.release(KEY_CAPS_LOCK);
+  }
+  if (axis == 7){
+    caps_lock_left = digitalRead(A0);
+  }
+}
+
+void DecimalToBinary(int n) {
+   int binaryNumber[] = {0,0,0};
+   int i = 0;
+   while (n > 0) {
+      binaryNumber[2 - i] = n % 2;
+      n = n / 2;
+      i++;
+   }
+
+   bits[0] = binaryNumber[0];
+   bits[1] = binaryNumber[1];
+   bits[2] = binaryNumber[2];
+}
